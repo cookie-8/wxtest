b5b68758ade748deaad27783d829fd15d2073c7a
hj <hj@qq.com>
2019-01-05 18:54:50 +0800

    wx

---
+++ pages/wx/wx.js
@@ -0,0 +1,75 @@
+// pages/wx/wx.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    te_title: "小购物车宣传"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function(res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '来自小柚子的分享',
+      path: '/pages/wx/wx'
+    }
+  },
+
+
+})
